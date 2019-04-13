dcecccd1886cbe6f80afc93b73e82c25464265a9
C3NZ <cenz@cenz.io>
2019-04-12 17:56:01 -0700

    [feat] update server

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 "use strict";
 
+// Setup app
 const express = require("express");
-
 const PORT = 8080;
 const HOST = "0.0.0.0";
-
 const app = express();
 
+// Configure root route
 app.get("/", (req, res) => {
   res.send("Hello world!\n");
 });
 
+// Setup server
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
